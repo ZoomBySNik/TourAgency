bc71a097c89f240fd1908932719386ca44b8f35d
ZoomBySNik <Onyx1504@yandex.ru>
2023-05-23 19:55:55 +0300

    Создана страница поиска туров, планируется доработать фильтры

--- pages/forms.py
+++ pages/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from tours.models import Customer
+from django.db.models import Count
+from tours.models import *
 
 
 class CustomerRegistrationForm(UserCreationForm):
@@ -10,4 +11,24 @@ class CustomerRegistrationForm(UserCreationForm):
 
     class Meta(UserCreationForm.Meta):
         model = Customer
-        fields = UserCreationForm.Meta.fields + ('phone_number','first_name', 'last_name')
+        fields = UserCreationForm.Meta.fields + ('phone_number', 'first_name', 'last_name')
+
+
+class FilterForTours(forms.Form):
+    min_price = forms.IntegerField(label='Цена от', required=False)
+    max_price = forms.IntegerField(label='Цена до', required=False)
+    min_date = forms.DateField(label='Даты с', required=False)
+    max_date = forms.DateField(label='Даты по', required=False)
+    type_of_resort = forms.MultipleChoiceField(label='Тип курорта', widget=forms.CheckboxSelectMultiple, required=False)
+    departure_city = forms.MultipleChoiceField(label='Город отправления', required=False)
+    resort_city = forms.MultipleChoiceField(label='Город назначения', required=False)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['type_of_resort'].choices = [(type_r.id, type_r.title) for type_r in TypeOfResort.objects.all()]
+        self.fields['departure_city'].choices = [(city_d.id, city_d.title)
+                                                 for city_d in
+                                                 City.objects.filter(id__in=Tour.objects.values_list('departure_city'))]
+        self.fields['resort_city'].choices = [(city_r.id, city_r.title)
+                                              for city_r in
+                                              City.objects.filter(hotel__isnull=False).annotate(num_hotels=Count('hotel'))]
--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,9 @@
+from sqlite3 import Date
+
 from tours.models import *
 from django.shortcuts import render, redirect
-from .forms import CustomerRegistrationForm
+from .forms import CustomerRegistrationForm, FilterForTours
+from decimal import Decimal
 
 
 def register_customer(request):
@@ -15,20 +18,46 @@ def register_customer(request):
     return render(request, 'registration/register.html', {'form': form})
 
 
-
 def home_view(request, *args, **kwargs):
     print(*args, **kwargs)
     tours = Tour.objects.all()
-    news = News.objects.all().order_by('departure_date')
+    news = News.objects.all().order_by('-departure_date')
     context = {
         'tours': tours,
         'news': news,
     }
     return render(request, "home.html", context)
 
+
 def news_view(request):
-    news = News.objects.order_by('departure_date').all()
+    news = News.objects.order_by('-departure_date').all()
     context = {
         'news':news
     }
     return render(request, "news.html", context)
+
+
+def tours_view(request):
+    tours = Service.objects.filter(tour__isnull=False)
+    form = FilterForTours(request.GET)
+    if form.is_valid():
+        if form.cleaned_data["min_price"]:
+            min_price = Decimal(form.cleaned_data["min_price"])
+            tours = tours.filter(price__gte=min_price)
+        if form.cleaned_data["max_price"]:
+            max_price = Decimal(form.cleaned_data["max_price"])
+            tours = tours.filter(price__lte=max_price)
+        if form.cleaned_data["min_date"]:
+            min_date = form.cleaned_data["min_date"]
+            tours = tours.filter(start_date__gte=min_date)
+        if form.cleaned_data["max_date"]:
+            max_date = form.cleaned_data["max_date"]
+            tours = tours.filter(end_date__lte=max_date)
+        if 'reset' in request.GET:
+            return redirect('tours_view')
+
+    context = {
+        'tours': tours,
+        'form': form,
+    }
+    return render(request, "tours.html", context)
